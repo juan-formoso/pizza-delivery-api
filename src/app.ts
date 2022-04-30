7bb73705d94c04319261ea144c71de1e19aaa5ed
Avery <trajano.juan@gmail.com>
2022-04-30 12:58:07 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import express from "express";
+import config from "config";
+import connection from "./utils/connection";
+import logger from "./utils/logger";
+import routes from "./routes";
+
+const port = config.get<number>("port");
+const app = express();
+
+app.listen(port, async () => {
+  logger.info(`App is running at http://localhost:${port}`);
+
+  await connection();
+
+  routes(app);
+});
