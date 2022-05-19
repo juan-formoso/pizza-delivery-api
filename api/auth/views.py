a73242919fa43fe05cea3aa162546b3c70f661a2
Avery <trajano.juan@gmail.com>
2022-05-19 11:52:52 -0300

    feat: :sparkles: Adiciona views.py em /api/auth
    Importa flask_restx; Cria classe HelloAuth.

---
+++ api/auth/views.py
@@ -0,0 +1,8 @@
+from flask_restx import Namespace, Resource
+
+auth_namespace=Namespace('auth', description="a namespace for authentication")
+
+@auth_namespace.route('/')
+class HelloAuth(Resource):
+  def get(self):
+    return {'message': 'Hello Auth'}
