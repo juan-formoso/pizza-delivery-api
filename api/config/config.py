40693bb635c3ac3979f06263efc16d66958473aa
Avery <trajano.juan@gmail.com>
2022-05-19 14:51:50 -0300

    feat: :sparkles: Cria config.py em /api/config
    Importa config de decouple; Cria classes Config, DevConfig, TestConfig e ProdConfig; Cria config_dict.

---
+++ api/config/config.py
@@ -0,0 +1,20 @@
+import os
+from decouple import config
+
+class Config:
+  SECRET_KEY=config('SECRET_KEY', 'secret')
+  
+class DevConfig(Config):
+  DEBUG=config('DEBUG', cast=bool)
+
+class TestConfig(Config):
+  pass
+
+class ProdConfig(Config):
+  pass
+
+config_dict={
+  'dev': DevConfig,
+  'test': TestConfig,
+  'prod': ProdConfig,
+}
