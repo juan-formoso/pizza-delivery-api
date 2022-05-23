adc1e56d3f26ab3bb97c77c587b8413e053fb5d9
Avery <trajano.juan@gmail.com>
2022-05-23 14:32:15 -0300

    Atualiza os Models

--- api/models/orders.py
+++ api/models/orders.py
@@ -1,6 +1,5 @@
-from ..utils import db
+from ..utils.db import db
 from enum import Enum
-from datetime import datetime
 
 class Sizes(Enum):
   SMALL = 'small'
@@ -14,14 +13,26 @@ class OrderStatus(Enum):
   DELIVERED = 'delivered'
 
 class Order(db.Model):
-  __tablename__='orders'
-  id=db.Column(db.Integer(), primary_key=True)
-  size=db.Column(db.Enum(Sizes), default=Sizes.SMALL)
-  order_status=db.Column(db.Enum(OrderStatus), default=OrderStatus.PENDING)
-  flavour=db.Column(db.String(), nullable=False)
-  date_created=db.Column(db.DateTime(), default=datetime.utcnow)
-  user=db.Column(db.Integer(), db.ForeignKey('users.id'))
+  __tablename__ = 'orders'
+
+  id = db.Column(db.Integer(), primary_key = True)
+  size = db.Column(db.Enum(Sizes), default = Sizes.SMALL)
+  order_status = db.Column(db.Enum(OrderStatus), default = OrderStatus.PENDING)
+  flavour = db.Column(db.String(), nullable = False)
+  quantity = db.Column(db.Integer())
+  customer = db.Column(db.Integer(), db.ForeignKey('users.id'))
   
   def __repr__(self):
     return f"<Order {self.id}>"
-  
+
+  def save(self):
+    db.session.add(self)
+    db.session.commit()
+
+  @classmethod
+  def get_by_id(cls,id):
+    return cls.query.get_or_404(id)
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
